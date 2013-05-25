f9084b454aebc74a9df30fcd5f0bfb4f703af76c
Fadi Adawi <fadi_adawi@hotmail.com>
2013-05-25 13:35:43 -0700

    test

---
+++ application/controllers/test.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of test
+ *
+ * @author fadiadawi
+ */
+class Test extends CI_Controller {
+    //put your code here
+}
+
+?>
